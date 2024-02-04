5e924494158db9ee0e28d2f0d8600cc48e8d9555
dev_one <micool1030@gmail.com>
2024-02-05 06:49:28 +0900

    [test] BeanDefinition test

---
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,27 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타 정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        assertThat(beanDefinitionNames).isNotEmpty();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinition = " + beanDefinition +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
