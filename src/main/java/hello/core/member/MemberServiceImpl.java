e1fde398039aa0088f14b76f728c0f3409bcc2ff
dev_one <micool1030@gmail.com>
2024-02-08 12:36:44 +0900

    [test] @Configuration 싱글톤 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,13 +1,11 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
 
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
-    private final MemberRepository memberRepository;
-
 
     @Override
     public void join(Member member) {
@@ -17,5 +15,10 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
